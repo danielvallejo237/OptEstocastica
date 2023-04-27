f5ea1953f53372c0b8dbdacb81817a687ffcf91d
danielvallejo237 <daniel.vallejo@cimat.mx>
2023-04-26 21:52:02 -0600

    excecutablebnp

---
+++ Tarea5/BNP-Secuencial/excecute.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cust_func()
+{
+    ./a.out N = $1 pc = $2 pm = 1 finalTime = 43200 seed = $RANDOM fileName = $3 outputFile = $4
+    sleep 1
+}
+
+g++ main.cpp MA.cpp Problem.cpp utils.cpp Individual.cpp -std=c++11 
+
+for i in {1..30}
+do
+    cust_func 50 0.95 "pmed1.txt" "bnp_pmed1_run_${i}" &
+done&&
+
+for i in {1..30}
+do
+    cust_func 50 0.95 "pmed9.txt" "bnp_pmed9_run_${i}" &
+done&&
+
+for i in {1..30}
+do
+    cust_func 50 0.95 "pmed15.txt" "bnp_pmed15_run_${i}" &
+done&&
+
+wait
